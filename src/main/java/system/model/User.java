08634ee7999f3055134878fd9f531676827a8bc1
Sergey Romanovich <sramanovich@gmail.com>
2017-10-20 17:16:50 +0300

    mysql

--- src/main/java/system/model/User.java
+++ src/main/java/system/model/User.java
@@ -1,9 +1,15 @@
 package system.model;
 
+import javax.persistence.*;
+import java.io.Serializable;
+
 /**
  * Created by Administrator on 18.10.2017.
  */
-public class User {
+@Entity
+@Table(name = "Users", schema = "", catalog = "UserSystem")
+public class User implements Serializable{
+    private int id;
     private String name;
     private String password;
 
@@ -16,6 +22,18 @@ public class User {
         this.password = password;
     }
 
+    @Id
+    @Column(name = "id", nullable = false, insertable = true, updatable = true)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "name", nullable = false, insertable = true, updatable = true, length = 60)
     public String getName() {
         return name;
     }
@@ -24,6 +42,8 @@ public class User {
         this.name = name;
     }
 
+    @Basic
+    @Column(name = "password", nullable = false, insertable = true, updatable = true, length = 20)
     public String getPassword() {
         return password;
     }
